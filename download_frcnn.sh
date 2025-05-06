0000438c1f620f4aff907d370bfe0b94bb656f27
feemthan <mohamedfaheemthanveer@gmail.com>
2025-05-06 02:33:54 +0100

    ensure models are correctly downloaded as hashes throw errors when called from python

---
+++ download_frcnn.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://download.pytorch.org/models/fasterrcnn_resnet50_fpn_coco-258fb6c6.pth
---
+++ download_yolov8.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://github.com/ultralytics/assets/releases/download/v8.3.0/yolov8n.pt
+wget https://github.com/ultralytics/assets/releases/download/v8.3.0/yolov8s.pt
+wget https://github.com/ultralytics/assets/releases/download/v8.3.0/yolov8m.pt
+wget https://github.com/ultralytics/assets/releases/download/v8.3.0/yolov8l.pt
+wget https://github.com/ultralytics/assets/releases/download/v8.3.0/yolov8x.pt
